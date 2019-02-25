3615248cf8cbe5d26147038cf001521ff03d4875
Ouandi Mbiandja Hoffer Fleury <hofferouandi@yahoo.fr>
2019-02-25 12:19:30 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ var fs = require('fs');
 
 // Chargement du fichier index.html affiché au client
 var server = http.createServer(function(req, res) {
-    fs.readFile('./index.html', 'utf-8', function(error, content) {
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(content);
-    });
+    // fs.readFile('./index.html', 'utf-8', function(error, content) {
+    //     res.writeHead(200, {"Content-Type": "text/html"});
+    //     res.end(content);
+    // });
+     res.send('Hello World!')
 });
 
 // Chargement de socket.io
